24095df13a0ab62ea4c907ad6fd9958b8ecd9539
makom <mako.mail.g@gmail.com>
2022-02-16 09:51:40 +0900

    HelloControllerに名前変更

--- src/main/java/com/makowis/spring/controller/HelloController.java
+++ src/main/java/com/makowis/spring/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Hello {
+public class HelloController {
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
