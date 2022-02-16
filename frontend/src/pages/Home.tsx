45e0763aba3e7980e2390b8f52bf8f7976cc1184
makom <mako.mail.g@gmail.com>
2022-02-16 17:12:17 +0900

    API経由で表示データを取ってくるように修正

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import useGetHelloWorld from "../hooks/use-get-helloworld";
 
 const Home: React.FC = () => {
-    return <h1>Home</h1>;
+    const { hello } = useGetHelloWorld();
+    return (
+        <>
+          <h1>Home</h1>
+          <p>{hello}</p>
+        </>
+        );
+
 };
 
 export default Home;
