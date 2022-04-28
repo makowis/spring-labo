f1e81ae8bdc4ac5e876f6a4044dbdee8e2978146
makom <mako.mail.g@gmail.com>
2022-04-28 10:49:01 +0900

    認証してないとアクセスできないAPIを増やしてみた

--- src/main/java/com/makowis/spring/config/MySecurityConfig.java
+++ src/main/java/com/makowis/spring/config/MySecurityConfig.java
@@ -3,14 +3,18 @@ package com.makowis.spring.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 @EnableWebSecurity
@@ -26,19 +30,39 @@ public class MySecurityConfig  extends WebSecurityConfigurerAdapter {
         // @formatter:off
         http
             .authorizeRequests()
-                .antMatchers(HttpMethod.GET,
-                        "/*", "/static/**").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/api/auth/*").authenticated()
+                .anyRequest().permitAll()
                 .and()
-            .formLogin()
-                .defaultSuccessUrl("/", true)
-                .permitAll()
-                .and()
-            .httpBasic()
-                .and()
-            .csrf().disable()
-            .logout()
-                .logoutSuccessUrl("/");
+            .csrf().disable();
+
+        // 参考:https://fintan.jp/page/373/
+        JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter =
+                new JsonUsernamePasswordAuthenticationFilter(authenticationManager());
+        jsonUsernamePasswordAuthenticationFilter.setUsernameParameter("email");
+        jsonUsernamePasswordAuthenticationFilter.setPasswordParameter("password");
+        // ログイン後にリダイレクトのリダイレクトを抑制
+        jsonUsernamePasswordAuthenticationFilter
+                .setAuthenticationSuccessHandler((req, res, auth) -> res.setStatus(HttpServletResponse.SC_OK));
+        // ログイン失敗時のリダイレクト抑制
+        jsonUsernamePasswordAuthenticationFilter
+                .setAuthenticationFailureHandler((req, res, ex) -> res.setStatus(HttpServletResponse.SC_UNAUTHORIZED));
+
+        // FormログインのFilterを置き換える
+        http.addFilterAt(jsonUsernamePasswordAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        // Spring Securityデフォルトでは、アクセス権限（ROLE）設定したページに未認証状態でアクセスすると403を返すので、
+        // 401を返すように変更
+        http.exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+        // 今回は、403エラー時にHTTP Bodyを返さないように設定
+        http.exceptionHandling().accessDeniedHandler((req, res, ex) -> res.setStatus(HttpServletResponse.SC_FORBIDDEN));
+
+        // ログアウト
+        http
+                .logout()
+                .logoutUrl("/logout")
+                // ログアウト時のリダイレクト抑制
+                .logoutSuccessHandler((req, res, auth) -> res.setStatus(HttpServletResponse.SC_OK))
+                .invalidateHttpSession(true);
         // @formatter:on
     }
 //
